8614a1ad6924989908a6bc68b7dd353faaa7b5f7
v074 <buncherd01@gmail.com>
2021-03-01 13:56:26 -0600

    Completing initial version of textGame.js

--- textGame.js
+++ textGame.js
@@ -35,12 +35,30 @@ ${enter}`;
 const gameOver5 = `You looked at the trap and the light BLINDED YOU! ${gameOver}`;
 const win = `The ghost trap sucked Slimer in and closed. CONGRATULATIONS! YOU CAUGHT SLIMER!!!`;
 
-let choice;
-
 alert(start);
-choice=prompt(q1);
-if (choice==1){
-
+let userInput=prompt(q1);
+if (userInput==1){
+    userInput=prompt(q2);
+    if(userInput==1){
+        userInput=prompt(q3);
+        if(userInput==2){
+            userInput=prompt(q4);
+            if(userInput==1){
+                userInput=prompt(q5);
+                if(userInput==2){
+                    alert(win);
+                }else{
+                    alert(gameOver5);
+                }
+            }else{
+                alert(gameOver4);
+            }
+        }else{
+            alert(gameOver3);
+        }
+    }else{
+        alert(gameOver2);
+    }
 }else{
-    alert (gameOver5);
+    alert (gameOver1);
 }
